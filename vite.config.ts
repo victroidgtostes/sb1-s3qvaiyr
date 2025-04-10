ee528f9c28b28e8b9cec6770d6c4ad3fbc9196e0
victroidgtostes <victorguimaraestostes@hotmail.com>
2025-04-09 22:57:45 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: './', 
+  base: './',
+  build: {
+    rollupOptions: {
+      external: ['swiper/css', 'swiper/css/navigation', 'swiper/css/pagination'],
+    },
+  },
 });
