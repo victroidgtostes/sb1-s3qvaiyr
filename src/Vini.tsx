0cac5b152587bd29899dcc6ce0c62a97f9e3592f
victroidgtostes <victorguimaraestostes@hotmail.com>
2025-04-18 03:43:20 -0300

    Update Vini.tsx

--- src/Vini.tsx
+++ src/Vini.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { motion } from "framer-motion";
 import React from "react";
 import { Swiper, SwiperSlide } from "swiper/react";
